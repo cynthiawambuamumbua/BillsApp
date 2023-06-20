dbf42ffb46256aefa59be17f0d4a91262b253580
priange <ityakup@gmail.com>
2023-06-20 15:43:32 +0300

    changes

--- app/src/main/java/com/example/appasessment/MainActivity.kt
+++ app/src/main/java/com/example/appasessment/MainActivity.kt
@@ -3,36 +3,56 @@ package com.example.appasessment
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatViewInflater
-import java.sql.Struct
+import com.example.appasessment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    var binding=AppCompatActivity.LAYOUT_INFLATER_SERVICE
+    lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=AppCompatActivity.LAYOUT_INFLATER_SERVICE
-        setContentView(binding.length)
-
-
+        setContentView(R.layout.activity_main)
+        binding =ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
-
     override fun onResume() {
         super.onResume()
-        currentFocus
-
+        setContentView(binding.root)
+        binding.btnNext.setOnClickListener {
+            validateRegistration()
+        }
     }
-    fun bills(UserName:String,phoneNumber: String,Email:String,Password:String,Image:String){
-        var name=bills("cynthia","0713504579","cynthia54@gmail.com","897987"," ")
-        var name2=bills("jeff","0721264170","jeff@gmail.com","67586"," ")
-        var name3=bills("mbula","0786865969","mbula@yahoo.com","089657"," ")
-        var name4=bills("muma","012367969","muma@gmail.com","907574"," ")
-        var name5=bills("lucky","012457687","luckwe@gmail.com","079869"," n")
-        var  names= mutableListOf(name,name2,name3,name4,name5)
+    fun validateRegistration() {
+        val name = binding.etusername.text.toString()
+        val phonenumber =binding.etphonenumber.text.toString()
+        val email=binding.etemail .text.toString()
+        val password =binding.etpassword .text.toString()
+        var error = false
+        if (name.isBlank()) {
+            binding.tilusername.error = "Name required"
+            error=true
+        }
+        if (name.isBlank()) {
+            binding.tilphonenumber.error = "Phonenumber required"
+            error=true
+        }
+        if (name.isBlank()) {
+            binding.tilemail.error = "Email required"
+            error=true
+        }
+        if (name.isBlank()) {
+            binding.tilpassword.error = "Password required"
+            error=true
+        }
+        if (!error){
+            val intent= Intent(this,login::class.java)
+            startActivity(intent)
+        }
     }
-
-
-
-
-
 }
+
+
+
+
+
+
+
